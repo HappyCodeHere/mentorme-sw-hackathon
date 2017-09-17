744a0fa673e74a763bb7e4b83edde9d72efd8e19
Ches Zhuravsky <happylolonly@gmail.com>
2017-09-17 15:21:27 +0300

    update landing

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -45,13 +45,33 @@ class RegistrationPage extends Component {
           />
         </header>
         <section>
-          секция 1
+          <div className="row">
+            <div className="col-md-5">
+              <img src="" alt="jkjkjk"/>
+            </div>
+            <div className="col-md-7">
+              <h4>Залоговок</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere pariatur mollitia omnis veritatis beatae veniam tempore excepturi. Temporibus neque alias enim praesentium, fugit cupiditate. Eius qui nam quod eum, accusamus sit. Aspernatur quidem, ab ad quibusdam quia doloribus nisi rerum asperiores modi provident ullam commodi fugit ipsam autem, soluta accusamus.</p>
+            </div>
+          </div>
         </section>
         <section>
-          секция 2
+          <div className="row">
+            <div className="col-md-6">
+              <h5>Заголовок</h5>
+              <img src="" alt="9090" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum necessitatibus modi, dicta quas at architecto cumque reiciendis sit iure rem vel commodi neque ipsum voluptates et, placeat adipisci in aliquid.</p>
+            </div>
+            <div className="col-md-6">
+              <h5>Заголовок</h5>
+              <img src="" alt="9090" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum necessitatibus modi, dicta quas at architecto cumque reiciendis sit iure rem vel commodi neque ipsum voluptates et, placeat adipisci in aliquid.</p>
+            </div>
+          </div>
         </section>
         <section>
-          секция 3
+          <img src="" alt="9090"/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, doloremque blanditiis, eveniet libero et culpa fugit maxime provident praesentium ea consectetur distinctio dicta, inventore incidunt tempore, obcaecati nam voluptates? Tempora aliquam possimus aperiam officiis, facilis eaque pariatur assumenda necessitatibus incidunt?</p>
         </section>
       </div>
     )
